553b5ecb108a176060f8aef6f2bd23d8bb04c9e1
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-21 17:36:44 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adriano Valumin on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
