2f88dd48cdf30e824690b44bbb616fbc660301e7
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-21 18:14:20 -0300

    Initial example codes

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,15 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var cats = ["Tony", "Calvin", "Minnie"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        List {
+//            ForEach(0..<5) {
+//                Section("Section \($0)") {
+//                    ForEach(0..<5) {
+//                        Text("Hello \($0)")
+//                    }
+//                }
+//            }
+//        }
+//        .listStyle(.insetGrouped)
+        
+//        List(0..<5) {
+//            Text("Hello, \($0)")
+//        }
+        
+        List(cats, id: \.self) {
+            Text($0)
+        }
+        
+        HStack {
+            
+            Spacer()
+            
+            Button("Add more") {
+                cats.append(cats.randomElement() ?? "Unknown")
+            }
+            .buttonStyle(.bordered)
+            
+            Spacer()
+            
+            Button("Remove") {
+                cats.removeLast()
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
         }
         .padding()
+        .border(.background)
+        
     }
+    
+//    func testBundles() {
+//        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt")
+//            if let fileContents = try? String(contentsOf: fileURL) {
+////            Loaded the file into a string
+//        }
+//    }
+    
 }
 
 #Preview {
